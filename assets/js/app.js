dfcf40e96ab8c349bedcb3f335f47c9acaa43f9d
Branford Shaw <branford.shaw@gmail.com>
2020-02-21 08:26:55 -0500

    brought in chartist for graphic in bio

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,3 +28,43 @@ var lastId,
 			return item;
 		}
 	});
+
+// var data = {
+// 	// A labels array that can contain any sort of values
+// 	labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+// 	// Our series array that contains series objects or in this case series data arrays
+// 	series: [[5, 2, 4, 2, 0]]
+// };
+
+// // Create a new line chart object where as first parameter we pass in a selector
+// // that is resolving to our chart container element. The Second parameter
+// // is the actual data object.
+// new Chartist.Bar(".ct-chart", data);
+
+new Chartist.Bar(
+	".ct-chart",
+	{
+		labels: ["MySQL", "Node.js", "Javascript", "CSS3", "HTML5"],
+		series: [[2, 3, 3.5, 4, 5]]
+	},
+	{
+		horizontalBars: true,
+		// showPoint: false,
+		// showLine: false,
+		// showArea: true,
+		// fullWidth: true,
+		// showLabel: true,
+		axisX: {
+			showGrid: false,
+			showLabel: false
+			// offset: 0
+		},
+		axisY: {
+			showGrid: false,
+			showLabel: true,
+			offset: 70
+		}
+		// chartPadding: 0,
+		// low: 0
+	}
+);
